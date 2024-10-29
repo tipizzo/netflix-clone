a907c5aad9b917fa4857fc3562f4d47e6fa3d349
KABANGA David <Davkabanga96@gmail.com>
2024-10-29 11:39:20 +0200

    Finalization of the netflix-clone home page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useRef} from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+const TitleCards = ({ title, category }) => {
 
   const cardsRef = useRef();
 
@@ -17,7 +17,7 @@ const TitleCards = () => {
 
   return (
     <div className='title-cards'>  
-      <h2>Popular on Netflix</h2>
+      <h2>{title?title:"Popular on Netflix"}</h2>
       <div className='card-list' ref={cardsRef}>
         {cards_data.map((card, index) => {
           return <div className='card' key={index}>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
@@ -25,6 +26,13 @@ const Home = () => {
           <TitleCards />
         </div>
       </div>
+      <div className='more-cards'>
+        <TitleCards title={"blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"} />
+        <TitleCards title={"Upcoming"} />
+        <TitleCards title={"Top Pics for You"} />
+      </div>
+      <Footer />
     </div>
   )
 }
